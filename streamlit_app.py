462b7893fe2443da404ec2314b094e0989f8d592
GUNTI SESHA SAI SADVIKA <158547366+sadviii@users.noreply.github.com>
2026-05-19 10:57:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,12 +47,12 @@ if ingredients_list:
 
     #st.write(ingredients_list)  
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+      my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                 values ('""" + ingredients_string + """','"""+Name_on_order+"""')"""
 
     #st.write(my_insert_stmt)
-    time_to_insert = st.button('Submit Order')
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
+     time_to_insert = st.button('Submit Order')
+     if time_to_insert:
+         session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered,' + Name_on_order + '!', icon="✅")
+         st.success('Your Smoothie is ordered,' + Name_on_order + '!', icon="✅")
